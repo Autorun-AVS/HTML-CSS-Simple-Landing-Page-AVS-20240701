4892821353b80f686ba5360d6f8ee58557b1b851
Autorun-AVS <102346021+Autorun-AVS@users.noreply.github.com>
2024-07-01 14:52:41 +0600

    updated to version 2

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+class CustomElementAVS extends HTMLElement {
+  constructor() {
+    super();
+    // Create shadow DOM if needed
+    // Add content, attributes, event listeners, etc.
+  }
+}
+customElements.define("x-card", CustomElementAVS);
+customElements.define("x-layout", CustomElementAVS);
+
+
+
+
+// #developerRight ! Hi there. This project is completely open for you. You can study it or use in your project. But I will appreciate if you at least give me the credit. Thanks, take care and stay safe. https://github.com/Autorun-AVS
